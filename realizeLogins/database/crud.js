21af1b1255c2d3dc9877ceda27498aa6d99a435a
Giovanni <854413241@qq.com>
2016-01-21 20:04:17 +0800

    登录注册全部都用回调实现

--- realizeLogins/database/crud.js
+++ realizeLogins/database/crud.js
@@ -3,7 +3,6 @@ var assert = require("assert"); //断言
 // Connection URL  nrsdb是新闻系统的数据库名字,确保连接之前要打开mongodb(mongod)
 var url = 'mongodb://localhost:27017/nrsdb';
 
-
 function connectToDatabase(request, response, paraToDatabase) {
 	console.log("我在connectToDatabase函数里面")
 	MongoClient.connect(url, function(err, db) {
@@ -36,7 +35,9 @@ function connectToDatabase(request, response, paraToDatabase) {
 			// 传送到ajax的信息
 			var statesToAjax = {
 				existedUserName: "isExistedUserName",
-				succeed: "succeed"
+				succeed: "succeed",
+				noUserName: "noUserName",
+				wrongPassword: "wrongPassword"
 			};
 			var collection = db.collection('users');
 			var uName = paraToDatabase.datas.post_uname;
@@ -54,8 +55,20 @@ function connectToDatabase(request, response, paraToDatabase) {
 					// 若是处理注册页面的
 					if (paraToDatabase.operation === "dealRegister") {
 						sendStatesToAjax(statesToAjax.existedUserName);
+					} else if (paraToDatabase.operation === "dealLogin") {
+						// 登录页面，且有账号,且密码相同
+						if (doc.password === uPassword) {
+							// 先异步设置cookie
+							var setCookie = require("../bin/manageCookie").setCookie;
+							setCookie(request, response, uName);
+							//然后再发送到ajax
+							sendStatesToAjax(statesToAjax.succeed);
+						} else {
+							sendStatesToAjax(statesToAjax.wrongPassword);
+						}
 					}
-				} else {
+				} else if (!doc && paraToDatabase.operation === "dealRegister") {
+					// 若是处理注册页面的且没有这个doc(用户名)
 					console.log("既没有报错也没有找到对应的doc,说明应该插入这个新账号");
 					var datasToAddAUser = {
 						collectionName: "users",
@@ -64,114 +77,17 @@ function connectToDatabase(request, response, paraToDatabase) {
 						statesToAjax: statesToAjax.succeed
 					}
 					database.addAUser(request, response, datasToAddAUser);
+				} else if (!doc && paraToDatabase.operation === "dealLogin") {
+					// 若是处理登录页面的且没有这个doc(用户名)
+					console.log("既没有报错也没有找到对应的doc,说明登录的账号不存在");
+					sendStatesToAjax(statesToAjax.noUserName);
 				}
 			});
 		}
-		if (paraToDatabase.operation === "dealRegister") {
-			database.userNameWhetherInDb(request, response, paraToDatabase);
-		}
+		database.userNameWhetherInDb(request, response, paraToDatabase);
 	});
 	// console.log("我在connectToDatabase函数最后面")
 	// return database;
 }
 
-// // 两个参数均为字符串类型
-// function addAUser(name, password) {
-// 	MongoClient.connect(url, function(err, db) {
-// 		assert.equal(null, err);
-// 		console.log("addAUser正确地连接到mongodb数据库");
-// 		// 建一个新表 Create a collection
-// 		var collection = db.collection('users');
-// 		var aUserData = {
-// 			name: name,
-// 			password: password
-// 		};
-// 		// 插入一个文档
-// 		// r是什么？？
-// 		collection.insertOne(aUserData, function(err, r) {
-// 				assert.equal(null, err);
-// 				assert.equal(1, r.insertedCount);
-// 			})
-// 			// 数据库插入完成
-// 			// 关闭数据库
-// 			// db.close();
-// 	});
-// }
-
-// function userNameWhetherInDb(uName, uPassword, callback) {
-// 	var result = false;
-// 	// 传送到ajax的信息
-// 	var statesToAjax = {
-// 		existedUserName: "isExistedUserName",
-// 		succeed: "succeed"
-// 	};
-// 	MongoClient.connect(url, function(err, db) {
-// 		console.log("userNameWhetherInDb 正确地连接到mongodb数据库");
-// 		var collection = db.collection('users');
-// 		console.log("开始查找");
-// 		collection.findOne({
-// 			name: uName
-// 		}, function(err, doc) {
-// 			if (err) {
-// 				console.log("报错了。");
-// 				console.log(err);
-// 			} else if (doc) {
-// 				// 若找到了这个doc
-// 				console.log("userNameWhetherInDb 找到了对应的doc");
-// 				callback(statesToAjax.existedUserName);
-// 			} else {
-// 				console.log("既没有报错也没有找到对应的doc,说明应该插入这个新账号");
-// 				addAUser(uName, uPassword);
-// 				callback(statesToAjax.succeed);
-// 			}
-// 		});
-// 		// console.log("马上关闭数据库")
-// 		// db.close();
-// 	});
-// 	return result;
-// }
-
-function loginFormUserNameWhetherInDb(uName, uPassword, callback) {
-	// 传送到ajax的信息
-	var statesToAjax = {
-		noUserName: "noUserName",
-		succeed: "succeed",
-		wrongPassword: "wrongPassword"
-	};
-	MongoClient.connect(url, function(err, db) {
-		console.log("loginFormUserNameWhetherInDb 正确地连接到mongodb数据库");
-		var collection = db.collection('users');
-		console.log("开始查找");
-		collection.findOne({
-			name: uName
-		}, function(err, doc) {
-			if (err) {
-				console.log("报错了。");
-				console.log(err);
-			} else if (doc) {
-				// 若找到了这个doc
-				console.log("loginFormUserNameWhetherInDb 找到了对应的doc,账户名存在");
-				console.log("数据库中这个doc的类型是:" + typeof doc);
-				console.log("数据库中这个doc(账户名对应的密码)是:" + doc.password);
-				if (doc.password === uPassword) {
-					console.log("与数据库中账户名密码均一致！");
-					// 这里多写一个uName参数，用于匹配成功之后，传到另外的函数进行加密操作
-					callback(statesToAjax.succeed, uName);
-				} else {
-					console.log("与数据库中账户名一致,但是密码不一致！");
-					callback(statesToAjax.wrongPassword);
-				}
-			} else {
-				console.log("既没有报错也没有找到对应的doc,说明没有这个账户名");
-				callback(statesToAjax.noUserName);
-			}
-		});
-		// console.log("马上关闭数据库")
-		// db.close();
-	});
-}
-// exports.addAUser = addAUser;
 exports.connectToDatabase = connectToDatabase;
-// exports.database = database;
-// exports.userNameWhetherInDb = userNameWhetherInDb;
-exports.loginFormUserNameWhetherInDb = loginFormUserNameWhetherInDb;
--- realizeLogins/routes/dealAjax.js
+++ realizeLogins/routes/dealAjax.js
@@ -9,7 +9,6 @@ function getData(request, response) {
   if (request.url != "/views/error.html") {
     return;
   }
-
   var body = '';
 
   request.on('data', function(data) {
@@ -39,13 +38,6 @@ function getData(request, response) {
     var connectToDatabase = require("../database/crud").connectToDatabase;
     //去数据库执行操作了：
     connectToDatabase(request, response, paraToDatabase);
-
-    // // states是string类型 传输状态返回到ajax
-    // function sendStatesToAjax(states) {
-    //   console.log("我是userNameWhetherInDb的回调函数,states为" + states);
-    //   response.end(states);
-    // }
-    // userNameWhetherInDb(post_uname, post_upassword, sendStatesToAjax);
   });
 }
 
@@ -59,8 +51,6 @@ function loginForm(request, response) {
 
   request.on('data', function(data) {
     body += data;
-    // Too much POST data, kill the connection!
-    // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
     if (body.length > 1e6)
       request.connection.destroy();
   });
@@ -68,29 +58,16 @@ function loginForm(request, response) {
   request.on('end', function() {
     // 解析字符串为对象
     var post = qs.parse(body);
-    // use post['blah'], etc.
-    // console.log(post.uname);
-    // console.log(post.upassword);
-    // 若ajax获取到了equire("../database/crud").addAUser;
-    // 调用数据库查询
-    var loginFormUserNameWhetherInDb = require("../database/crud").loginFormUserNameWhetherInDb;
-
-    console.log("post.uname的类型是" + typeof post.uname);
-    var post_uname = post.uname;
-    var post_upassword = post.upassword;
-    // states是string类型 传输状态返回到ajax
-    function sendStatesToAjax(states, uName) {
-      console.log("我是loginFormUserNameWhetherInDb的回调函数,states为" + states);
-      // 若states为 "succeed" 则： ajax返回之前，应执行user加密、写入cookie到浏览器的函数
-      if (states === "succeed") {
-        // 设置cookie方法
-        var setCookie = require("../bin/manageCookie").setCookie;
-        setCookie(request, response, uName);
-      }
-      response.end(states);
-    }
-    // 数据库做完操作之后执行回调函数sendStatesToAjax
-    loginFormUserNameWhetherInDb(post_uname, post_upassword, sendStatesToAjax);
+    var paraToDatabase = {
+      datas: {
+        post_uname: post.uname,
+        post_upassword: post.upassword,
+      },
+      operation: "dealLogin"
+    };
+    var connectToDatabase = require("../database/crud").connectToDatabase;
+    //去数据库执行操作了：
+    connectToDatabase(request, response, paraToDatabase);
   });
 }
 
